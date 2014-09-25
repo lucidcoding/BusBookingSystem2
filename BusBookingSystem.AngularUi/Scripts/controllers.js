568e7e4d6d62c7b66e5bd99838c077979b29a0fb
Paul Davies <pd_account_2@hotmail.com>
2014-09-25 10:33:34 +0100

    Datepicker added
    Added directive to use JQuery datepicker.

--- BusBookingSystem.AngularUi/Scripts/controllers.js
+++ BusBookingSystem.AngularUi/Scripts/controllers.js
@@ -109,5 +109,26 @@ controller('bookingAddController', function ($scope, $routeParams, bookingServic
 
         bookingService.save(data);
     };
+}).
+directive("datepicker", function () {
+    return {
+        restrict: "A",
+        require: "ngModel",
+        link: function (scope, elem, attrs, ngModelCtrl) {
+            var updateModel = function (dateText) {
+                scope.$apply(function () {
+                    ngModelCtrl.$setViewValue(dateText);
+                });
+            };
+            var options = {
+                dateFormat: "dd/mm/yy",
+                onSelect: function (dateText) {
+                    updateModel(dateText);
+                }
+            };
+            $(elem).datepicker(options);
+            //alert('this works');
+        }
+    }
 });
 
